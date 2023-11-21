fca5d7c86c293ac876aa7241bf3923bb325dab38
Quang Vu <ngqangvvu@gmail.com>
2023-11-21 14:12:53 +0700

    Fix api detail hotel

--- middlewares/hotels.js
+++ middlewares/hotels.js
@@ -3,6 +3,12 @@ import { LOCAL_API_URL } from "../src/api";
 export const GetAllHotels = async () => {
   const API = `${LOCAL_API_URL}/hotels/getall`;
   try {
+    const response = await fetch(API, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     const data = await response.json();
     return {status: 200, data: data};
   } catch (error) {
