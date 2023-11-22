dd7bbb0ad4fd4e8be2bac9a560e23743e0462cc1
nguyendinhduy13 <20521236@gm.uit.edu.vn>
2023-11-22 23:09:25 +0700

    update booking and add detail booking

--- src/views/Booking/Booking.js
+++ src/views/Booking/Booking.js
@@ -97,6 +97,363 @@ export default function Booking() {
         });
     };
 
+    const CardBooking = ({ item, index }) => {
+        return (
+            <TouchableOpacity
+                key={index}
+                onPress={() => {
+                    navigation.navigate('DetailBooking', {
+                        id_booking: item._id,
+                        id_hotel: item.id_hotel,
+                    });
+                }}
+                style={{
+                    backgroundColor: colors.box,
+                    borderRadius: 15,
+                    marginTop: 10,
+                    shadowColor: '#000',
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                    elevation: 5,
+                    width: '93%',
+                    alignSelf: 'center',
+                }}>
+                <View style={{ flexDirection: 'row', padding: 10 }}>
+                    <Image
+                        source={{ uri: filterHotel(item.id_hotel).image[0] }}
+                        style={{ width: 100, height: 100, borderRadius: 10 }}
+                    />
+                    <View style={{ marginLeft: 10, width: '100%' }}>
+                        <Text
+                            style={{
+                                fontSize: 20,
+                                color: colors.text,
+                                fontWeight: 'bold',
+                            }}>
+                            {filterHotel(item.id_hotel).name}
+                        </Text>
+                        <Text
+                            style={{
+                                marginTop: 5,
+                                width: 220,
+                                fontSize: 15,
+                                color: colors.icon,
+                            }}>
+                            {FormatName(item.id_room.name)}
+                        </Text>
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                marginTop: 'auto',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                width: 230,
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: '700',
+                                    color: colors.text,
+                                }}>
+                                {Format(item.total)}{' '}
+                                <Text style={{ fontSize: 13 }}>VNĐ</Text>
+                            </Text>
+                            <View
+                                style={{
+                                    padding: 5,
+                                    borderRadius: 10,
+                                    borderWidth: 1,
+                                    borderColor: colors.primary,
+                                }}>
+                                <Text
+                                    style={{
+                                        fontSize: 13,
+                                        color: colors.primary,
+                                    }}>
+                                    {filterStatus(item)}
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        paddingHorizontal: 10,
+                        paddingBottom: 10,
+                        width: '100%',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}>
+                    <View>
+                        <Text style={{ fontSize: 13, color: colors.icon }}>
+                            {t('check-in')}:{' '}
+                            <Text
+                                style={{
+                                    fontWeight: 'bold',
+                                    color: colors.text,
+                                }}>
+                                {FormatDayMonthYear(item.check_in)}
+                            </Text>
+                        </Text>
+                        <Text style={{ fontSize: 13, color: colors.icon }}>
+                            {t('check-out')}:{' '}
+                            <Text
+                                style={{
+                                    fontWeight: 'bold',
+                                    color: colors.text,
+                                }}>
+                                {FormatDayMonthYear(item.check_out)}
+                            </Text>
+                        </Text>
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                        }}>
+                        <TouchableOpacity
+                            style={{
+                                backgroundColor: colors.primary,
+                                padding: 10,
+                                borderRadius: 10,
+                                width: 90,
+                            }}
+                            onPress={() => {
+                                setModalVisible(true);
+                                setidroom(item._id);
+                            }}>
+                            <Text
+                                style={{ color: 'white', textAlign: 'center' }}>
+                                {t('cancel')}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    const CardCompleted = ({ item, index }) => {
+        return (
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.navigate('DetailBooking', {
+                        id_booking: item._id,
+                        id_hotel: item.id_hotel,
+                    });
+                }}
+                key={index}
+                style={{
+                    backgroundColor: colors.box,
+                    borderRadius: 15,
+                    marginTop: 10,
+                    shadowColor: '#000',
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                    elevation: 5,
+                    width: '93%',
+                    alignSelf: 'center',
+                }}>
+                <View style={{ flexDirection: 'row', padding: 10 }}>
+                    <Image
+                        source={{ uri: filterHotel(item.id_hotel).image[0] }}
+                        style={{ width: 100, height: 100, borderRadius: 15 }}
+                    />
+                    <View style={{ marginLeft: 10, width: '100%' }}>
+                        <Text
+                            style={{
+                                fontSize: 20,
+                                color: colors.text,
+                                fontWeight: 'bold',
+                            }}>
+                            {filterHotel(item.id_hotel).name}
+                        </Text>
+                        <Text
+                            style={{
+                                marginTop: 5,
+                                width: 220,
+                                fontSize: 15,
+                                color: colors.icon,
+                            }}>
+                            {FormatName(item.id_room.name)}
+                        </Text>
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                marginTop: 'auto',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                width: 230,
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: '700',
+                                    color: colors.text,
+                                }}>
+                                {Format(item.total)}{' '}
+                                <Text style={{ fontSize: 13 }}>VNĐ</Text>
+                            </Text>
+                            <View
+                                style={{
+                                    padding: 5,
+                                    borderRadius: 10,
+                                    borderWidth: 1,
+                                    borderColor: colors.primary,
+                                }}>
+                                <Text
+                                    style={{
+                                        fontSize: 13,
+                                        color: colors.primary,
+                                    }}>
+                                    {t('completed')}
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        backgroundColor: colors.blurprimary,
+                        padding: 10,
+                        borderRadius: 15,
+                        marginHorizontal: 10,
+                        marginBottom: 10,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}>
+                    <Icon1 size={20} name="checkbox" color={colors.primary} />
+                    <Text
+                        style={{
+                            fontSize: 14,
+                            fontWeight: '600',
+                            color: colors.primary,
+                            marginLeft: 10,
+                        }}>
+                        {t('great-you-have-completed-your-booking')}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    const CardCancelled = ({ item, index }) => {
+        return (
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.navigate('DetailBooking', {
+                        id_booking: item._id,
+                        id_hotel: item.id_hotel,
+                    });
+                }}
+                key={index}
+                style={{
+                    backgroundColor: colors.box,
+                    borderRadius: 15,
+                    marginTop: 10,
+                    shadowColor: '#000',
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                    elevation: 5,
+                    width: '93%',
+                    alignSelf: 'center',
+                }}>
+                <View style={{ flexDirection: 'row', padding: 10 }}>
+                    <Image
+                        source={{ uri: filterHotel(item.id_hotel).image[0] }}
+                        style={{ width: 100, height: 100, borderRadius: 15 }}
+                    />
+                    <View style={{ marginLeft: 10, width: '100%' }}>
+                        <Text
+                            style={{
+                                fontSize: 20,
+                                color: colors.text,
+                                fontWeight: 'bold',
+                            }}>
+                            {filterHotel(item.id_hotel).name}
+                        </Text>
+                        <Text
+                            style={{
+                                marginTop: 5,
+                                width: 220,
+                                fontSize: 15,
+                                color: colors.icon,
+                            }}>
+                            {FormatName(item.id_room.name)}
+                        </Text>
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                marginTop: 'auto',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                width: 230,
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: '700',
+                                    color: colors.text,
+                                }}>
+                                {Format(item.total)}{' '}
+                                <Text style={{ fontSize: 13 }}>VNĐ</Text>
+                            </Text>
+                            <View
+                                style={{
+                                    padding: 5,
+                                    borderRadius: 10,
+                                    borderWidth: 1,
+                                    borderColor: '#f76e64',
+                                }}>
+                                <Text
+                                    style={{
+                                        fontSize: 13,
+                                        color: '#f76e64',
+                                    }}>
+                                    {t('cancelled')}
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        backgroundColor: '#fddbdb',
+                        padding: 10,
+                        borderRadius: 15,
+                        marginHorizontal: 10,
+                        marginBottom: 10,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}>
+                    <Icon2 size={20} name="exclamationcircle" color="#f76e64" />
+                    <Text
+                        style={{
+                            fontSize: 14,
+                            fontWeight: '600',
+                            color: '#f76e64',
+                            marginLeft: 10,
+                        }}>
+                        {t('sorry-your-booking-has-been-cancelled')}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
     const dataPending = dataBooking?.filter(item => item.status === 'Pending');
     const [show, setshow] = useState(10);
     const dataCompleted = dataBooking
